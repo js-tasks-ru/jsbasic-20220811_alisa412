31e0976e57b5835aacb929fbfddb59d4bcfeff25
Alisa <kiselevaalisa99@gmail.com>
2022-08-30 17:52:07 +0200

    modul 3 task 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
 function camelize(str) {
-  // ваш код...
+  // ваш код..
+
+  let bla = str.split("-").map((item, index) => {
+    if (index === 0) {
+      //?
+      return item;
+    }
+    return item[0].toUpperCase() + item.slice(1);
+  });
+  return (bla = bla.join(""));
 }
+
+/*
+console.log(camelize("-webkit-transition"));
+*/
