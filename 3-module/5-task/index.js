795500f54ae6273b1d84200408a29d05e3049964
Alisa <kiselevaalisa99@gmail.com>
2022-09-05 22:22:22 +0200

    3 module, task 5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
 function getMinMax(str) {
-  // ваш код...
+  arrNumbers = [];
+  arrStr = str.split(" ");
+  arrStr.map((item, index) => {
+    if (isNaN(parseInt(arrStr[index])) !== true) {
+      arrNumbers.push(arrStr[index]);
+    }
+  });
+
+  let min = Math.min.apply(null, arrNumbers);
+  let max = Math.max.apply(null, arrNumbers);
+
+  return { min, max };
 }
+
+console.log(getMinMax("hi 33 223 hi"));
